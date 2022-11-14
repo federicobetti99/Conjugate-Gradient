47beaf62f76a8d143cd091dc69bb2a2dc961cf20
Federico Betti <74817555+federicobetti99@users.noreply.github.com>
2022-11-14 16:33:55 +0100

    ...

--- code/cg_main.cc
+++ code/cg_main.cc
@@ -7,7 +7,7 @@ using clk = std::chrono::high_resolution_clock;
 using second = std::chrono::duration<double>;
 using time_point = std::chrono::time_point<clk>;
 
-void partition_matrix(int N, int psize, std::vector<int> start_rows, std::vector<int> offsets_lengths)
+void partition_matrix(int N, int psize, int start_rows[], int offsets_lengths[])
 {
     start_rows.clear();
     start_rows.reserve(psize+1);
@@ -57,8 +57,8 @@ int main(int argc, char ** argv) {
     int n = solver.n();
     int m = solver.m();
 
-    std::vector<int> start_rows;
-    std::vector<int> offsets_lengths;
+    int start_rows(psize+1);
+    int offsets_lengths(psize+1);
     partition_matrix(m, psize, start_rows, offsets_lengths);
 
     double h = 1. / n;
