08ca19c0fbc0e3d421645ed2d511ef4b318a39ce
Federico Betti <74817555+federicobetti99@users.noreply.github.com>
2022-12-13 10:31:09 +0100

    Update cg.hh

--- code_CUDA/cg.hh
+++ code_CUDA/cg.hh
@@ -8,26 +8,29 @@
 #ifndef __CG_HH__
 #define __CG_HH__
 
-
-class Solver {
+class CGSolver
+{
 public:
+    /// initialize solver
+    CGSolver() = default;
+
     /// read matrix from .mtx file
-    virtual void read_matrix(const std::string & filename) = 0;
+    void read_matrix(const std::string & filename);
 
     /// initialize source term
-    virtual void init_source_term(double h) = 0;
+    void init_source_term(double h);
 
-    /// initialize size of the matrix (in this case m = n)
-    inline int m() const { return m_m; }
-    inline int n() const { return m_n; }
+    /// get submatrix for parallel computation
+    Matrix get_submatrix(int N_loc, int start_m);    
 
-    /// prescribe residual tolerance for ending of the algorithm
-    void tolerance(double tolerance) { m_tolerance = tolerance; }
+    /// get subvector for parallel computation
+    std::vector<double> get_subvector(std::vector<double>& arr, int N_loc, int start_m);
 
-    virtual void kerneled_solve(double *x, dim3 block_size) = 0;
+    /// solve linear system with iterative CG
+    void kerneled_solve(double *x, dim3 block_size);
 
-    virtual std::tuple<double, bool> cg_step_kernel(double* Ap, double* p, double* r, double *x,
-                                                    double rsold, dim3 grid_size, dim3 block_size) = 0;
+    std::tuple<double, bool> cg_step_kernel(double* Ap, double* p, double* r, double* x,
+                                  double rsold, dim3 grid_size, dim3 block_size);
 
 protected:
     /// initialize m and n
@@ -39,31 +42,7 @@ protected:
 
     /// residual tolerance
     double m_tolerance{1e-10};
-};
-
-class CGSolver : public Solver {
-public:
-    /// initialize solver
-    CGSolver() = default;
-
-    /// read matrix from .mtx file
-    virtual void read_matrix(const std::string & filename);
-
-    /// initialize source term
-    virtual void init_source_term(double h);
-
-    /// get submatrix for parallel computation
-    Matrix get_submatrix(int N_loc, int start_m);    
-
-    /// get subvector for parallel computation
-    std::vector<double> get_subvector(std::vector<double>& arr, int N_loc, int start_m);
-
-    /// solve linear system with iterative CG
-    virtual void kerneled_solve(double *x, dim3 block_size);
-
-    virtual std::tuple<double, bool> cg_step_kernel(double* Ap, double* p, double* r, double* x,
-                                  double rsold, dim3 grid_size, dim3 block_size);
-
+    
 private:
     /// finite element matrix
     Matrix m_A;
