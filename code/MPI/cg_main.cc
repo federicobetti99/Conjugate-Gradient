8181511ba92fa7f9bcf8d4430fcdb0bf7dbfeaaf
Federico Betti <federico.betti@epfl.ch>
2023-01-08 16:01:55 +0100

    .

--- code/MPI/cg_main.cc
+++ code/MPI/cg_main.cc
@@ -11,8 +11,7 @@ using time_point = std::chrono::time_point<clk>;
 
 
 int main(int argc, char ** argv) {
-    int provided;	
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
+    MPI_Init(&argc, &argv);
 
     /// MPI: Initialize and get rank
     int prank, psize;
